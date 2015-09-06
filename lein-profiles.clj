e992b2ca79a9f2ebc0cb4b21cfa326c027036712
Daniel Lobato <elobatocs@gmail.com>
2015-09-06 20:53:00 +0200

    Add lein profiles

---
+++ lein-profiles.clj
@@ -0,0 +1,7 @@
+{:user
+ {:plugins [[lein-exec "0.3.5"]
+            [venantius/ultra "0.3.3"]
+            [lein-ancient "0.6.7"]
+            [lein-kibit "0.1.2"]]
+  :ultra {:color-scheme :solarized_dark}}}
+
